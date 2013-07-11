f48a787886ec4b57d7e847c67742e0f5f65d650f
zhoulinlin <zhoulinlin@cnnic.cn>
2013-07-11 15:53:48 +0800

    errorfilter initial

---
+++ source/whois/src/com/cnnic/whois/web/ErrorFilter.java
@@ -0,0 +1,47 @@
+package com.cnnic.whois.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ErrorFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		
+		String path = request.getContextPath();
+//		if(){
+//			
+//		}
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void errorResponseFormat(){
+		
+	}
+}
