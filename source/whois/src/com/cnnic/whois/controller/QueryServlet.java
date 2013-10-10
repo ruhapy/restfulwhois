96ce9e4624b1784f10b6173ec48d2ec44cfc5d94
nic <nic@cnnic>
2013-10-10 23:33:55 +0800

    test

--- source/whois/src/com/cnnic/whois/controller/QueryServlet.java
+++ source/whois/src/com/cnnic/whois/controller/QueryServlet.java
@@ -15,9 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import com.cnnic.whois.execption.QueryException;
 import com.cnnic.whois.execption.RedirectExecption;
 import com.cnnic.whois.service.QueryService;
