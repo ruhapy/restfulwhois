fbc8768066ca3369384907c4a31ed2b06e831ae0
larrychen <chenxufu8276@gmail.com>
2013-12-27 09:21:12 +0800

    modify dao

--- source/whois/src/com/cnnic/whois/dao/oauth/UserAppDao.java
+++ source/whois/src/com/cnnic/whois/dao/oauth/UserAppDao.java
@@ -1,24 +1,69 @@
 package com.cnnic.whois.dao.oauth;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
 import com.cnnic.whois.bean.oauth.UserApp;
+import com.cnnic.whois.bean.oauth.UserAppRowMapper;
+import com.cnnic.whois.dao.BaseJdbcDao;
 
-public interface UserAppDao {
+@Repository
+public class UserAppDao extends BaseJdbcDao {
 
-	public void save(UserApp userApp);
+	public void save(UserApp userApp) {
+//		TODO : this part need modification, default invalid_time setting 7 days
+		userApp.setApp_key("key" +System.currentTimeMillis());
+		userApp.setApp_secret("secret" +System.currentTimeMillis());
+		userApp.setInvalid_time(7);
 
-	public void update(int id, UserApp userApp);
-
-	public void delete(int id);
+		String sql = "insert into oauth_users_app (app_key, app_secret, app_description, user_id, invalid_time) values(?, ?, ?, ?, ?)";
+		Object[] param = new Object[]{userApp.getApp_key(), userApp.getApp_secret(), userApp.getApp_description(), userApp.getUser_id(), userApp.getInvalid_time() };
+		this.getJdbcTemplate().update(sql, param);
+	}
 	
-	public UserApp getUserAppById(int id);
-
-	public UserApp getUserAppByUserId(int user_id);
-
-	public List<UserApp> getUserApps(int user_id);
-
-	public Map<String, String> getUserApps();
+	public void update(int id, UserApp userApp) {
+		String sql = "update oauth_users set app_description=? where id = ?";
+		Object[] param = new Object[]{userApp.getApp_description(), id };
+		this.getJdbcTemplate().update(sql, param);
+	}
 	
+	public void delete(int id) {
+		String sql = "delete from oauth_users_app where id =?";
+		Object[] param = new Object[]{id };
+		this.getJdbcTemplate().update(sql, param);
+	}
+
+	public UserApp getUserAppById(int user_id) {
+		String sql = "select id, app_key, app_secret, app_description, user_id from oauth_users_app where id = ? ";
+		Object[] param = new Object[] {user_id };
+		return this.getJdbcTemplate().queryForObject(sql, param, new UserAppRowMapper());
+	}
+	
+	public UserApp getUserAppByUserId(int user_id) {
+		String sql = "select id, app_key, app_secret, app_description, user_id from oauth_users_app where user_id = ? ";
+		Object[] param = new Object[] {user_id };
+		return this.getJdbcTemplate().queryForObject(sql, param, new UserAppRowMapper());
+	}
+
+	public List<UserApp> getUserApps(int user_id) {
+		String sql = "select id, app_key, app_secret, app_description, user_id from oauth_users_app where user_id = ? ";
+		Object[] param = new Object[] {user_id };
+		return this.getJdbcTemplate().query(sql, param, new UserAppRowMapper());
+	}
+
+	public Map<String, String> getUserApps() {
+		String sql = "select id, app_key, app_secret, app_description, user_id from oauth_users_app ";
+		List<UserApp> userApps = this.getJdbcTemplate().query(sql, new UserAppRowMapper());
+		Map<String, String> map = new HashMap<String, String>();
+		for(UserApp uApp : userApps){
+			uApp.getApp_description();
+			map.put(uApp.getApp_key(), uApp.getApp_secret());
+			map.put(uApp.getApp_key()+".description", uApp.getApp_description());
+		}
+		return map;
+	}
+
 }
